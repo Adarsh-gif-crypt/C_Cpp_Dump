7124b7c0fcfd4ada877f88f2ee5ff23edef5597c
Adarsh Shailendra <66865089+Adarsh-gif-crypt@users.noreply.github.com>
2022-06-28 16:03:00 +0530

    Printing functionality to be added

--- LinkedListDump/insertion.c
+++ LinkedListDump/insertion.c
@@ -6,7 +6,7 @@ struct node{
     struct node *link;
 };
 
-void insert_at_end(struct node *head, int data)
+void add_at_end(struct node *head, int data)
 {
     struct node *ptr, *temp;
     ptr = head;
@@ -21,12 +21,62 @@ void insert_at_end(struct node *head, int data)
     ptr->link = temp;
 }
 
-struct node *add_at_beg(struct node *head, int data){
+void *add_at_beg(struct node **head, int data){
     struct node *ptr = malloc(sizeof(struct node));
     ptr->data=data;
     ptr->link = NULL;
 
-    ptr->link = head;
-    head = ptr;
-    return head;
+    ptr->link = *head;
+    *head = ptr;
+}
+void add_to_pos(struct node *head, int data, int pos)
+{
+    struct node *ptr = head;
+    struct node *ptr1 = malloc(sizeof(struct node));
+    ptr1->data = data;
+    ptr1->link = NULL;
+
+    pos--;
+    while(pos!=1)
+    {
+        ptr = ptr->link;
+        pos--;
+    }
+    ptr1->link=ptr->link;
+    ptr->link=ptr1;
+}
+
+
+int main(){
+
+struct node *head = malloc(sizeof(struct node));
+printf("Enter 1 to add at beginning\n");
+printf("Enter 2 to add at the end\n");
+printf("Enter 3 to add at a certain position");
+int val;
+int data;
+scanf("%d",&val);
+if(val==1){
+    printf("Enter Data to be added: ");
+    scanf("%d",&data);
+    add_at_beg(head,data);
+}
+else if(val==2){
+    printf("Enter Data to be added: ");
+    scanf("%d",&data);
+    add_at_end(head, data);
+}
+else if(val == 3){
+    int pos;
+    printf("Enter Data to be added: ");
+    scanf("%d",&data);
+    printf("Position of the addition: ");
+    scanf("%d",&pos);
+    add_to_pos(head,data,pos);
+}
+else{
+    printf("Select Valid option!");
+}
+return 0;
+
 }
